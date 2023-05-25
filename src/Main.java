a00331030c244392a0bac479d9cb6506058b6644
Edmar Evangelista <eedmar42@yahoo.com,ph>
2023-05-26 07:18:52 +0800

    Created

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter the Two numbers for addition: ");
+        Scanner readInput = new Scanner(System.in);
+        int a = readInput.nextInt();
+        int b = readInput.nextInt();
+        readInput.close();
+
+        System.out.println("The sum of a and b is " + Integer.sum(a,b));
+    }
+}
